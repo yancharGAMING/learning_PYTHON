45ac3fedd407bc4a5651d493f65cf73c46671c57
Muslim Choriyev <choriyevmuslim@mail.ru>
2023-06-20 06:54:08 +0500

    nesting
    nesting qiynadi lekin mazza qildim bosh qotirib ;)

---
+++ lugat_bn_ishlash.py
@@ -0,0 +1,62 @@
+# Telegram BOT uchun harakat
+#yancharGAMING
+
+#lug'at
+
+#jalol = {'ism':'JaLol', 'yil':1999, 'joy':'Buxoro', 'yosh':24}
+#anvar = {'ism':'Anvar', 'yil':1998, 'joy':'andijon', 'yosh':25}
+#said = {'ism':'SaId', 'yil':1997, 'joy':'toshkent', 'yosh':26}
+#zohid = {'ism':'zoHId', 'yil':2000, 'joy':'qashqadaryo', 'yosh':23}
+#print(f"{jalol['ism'].title()} {jalol['yil']} da, {jalol['joy'].title()} da tug'ilgan, va hozir {jalol['yosh']}da.")
+#print(f"{anvar['ism'].title()} {anvar['yil']} da, {anvar['joy'].title()} da tug'ilgan, va hozir {anvar['yosh']}da.")
+#print(f"{said['ism'].title()} {said['yil']} da, {said['joy'].title()} da tug'ilgan, va hozir {said['yosh']}da.")
+#print(f"{zohid['ism'].title()} {zohid['yil']} da, {zohid['joy'].title()} da tug'ilgan, va hozir {zohid['yosh']}da.")
+
+#royxat = {'jalol':'osh', 'shoxrux':'somsa', 'aziz':'manti', 'muslim':'kefir'}
+#print(f"Jalolning sevimli ovqati {royxat['jalol']}.\
+#      Shoxruxning sevimli ovqati {royxat['shoxrux']}.\
+#      Azizning sevimli ovqati {royxat['aziz']}.")
+
+#lugat ={'float':"o'nlik son", 'int':'Butun son', 'string':'matn',
+#        'if':'agar', 'else':'yana', 'print':'chop etish', 'del':"o'chirish",
+#        'input':'kirit', 'upper':'katta', 'title':'titul'}
+#kalit = input("Kalit so'z kiriting:\t")
+#if kalit.lower() in lugat:
+#    print(f"{kalit.title()} so'zining manosi {lugat[kalit.lower()]}")
+#else: print("Bunday so'z mavjud emas")
+
+#lugat ={'float':"o'nlik son", 'int':'Butun son', 'string':'matn',
+#        'if':'agar', 'else':'yana', 'print':'chop etish', 'del':"o'chirish",
+#        'input':'kirit', 'upper':'katta', 'title':'titul'}
+#for soz in lugat:
+#    print(f"{soz.title()} - {lugat[soz].title()}")
+
+davlatlar = {
+    'aqsh' : 'washington d.c.',
+    'italya' : 'rim',
+    'malayziya' : 'kuala-lumpur',
+    "o'zbekiston" : 'toshkent',
+    "qirg'iziston" : 'bishkek',
+    "qozog'izton" : "nursulton",
+    'rossiya' : 'moskva',
+    'singapur' : 'sungapur',
+    'tojikiston' : 'dushanbe'}
+#print('Dunyo davlatlari:')
+#for davlat in sorted(davlatlar):
+#    print(f"{davlat.upper()}")
+#print('Davlatlarning poytaxtlari:')
+#for poytaxt in sorted(davlatlar.values()):
+#    print(f"{poytaxt.upper()}")
+#davlat = input("Qaysi davlatning poytaxtini bilishni istaysiz?\t")
+#if davlat.lower() in davlatlar:
+#    print(f"{davlat.upper()}ning poytaxti {davlatlar[davlat.lower()].title()} sahri.")
+#else: print("Bizda bunday malumot yo'q.")
+
+
+
+
+
+
+
+
+
